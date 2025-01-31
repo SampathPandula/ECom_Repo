87d9285ced188e3560d61c2beb838d9c8114889d
sampath <sampathsams319@gmail.com>
2025-01-31 10:15:54 +0530

    secound commit

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-00bb6a80f01f03502" # Change to a valid AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformInstance"
+  }
+}
+
+
+
