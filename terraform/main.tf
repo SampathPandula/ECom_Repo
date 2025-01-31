74f0be9ffef7a1927d16587b75d3ad55d1bdae77
SampathPandula <155477144+SampathPandula@users.noreply.github.com>
2025-01-31 10:10:32 +0530

    Update main.tf
    My_Demo_WebHook

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,12 @@
 provider "aws" {
-  region  = "ap-south-1"
-  profile = "default"
+  region = "ap-south-1"
 }
 
-resource "aws_vpc" "ecommerce_vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+resource "aws_instance" "example" {
+  ami           = "ami-00bb6a80f01f03502" # Change to a valid AMI
+  instance_type = "t2.micro"
 
-resource "aws_subnet" "subnet_a" {
-  vpc_id                  = aws_vpc.ecommerce_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"  # First AZ
-  map_public_ip_on_launch = true
-}
-
-resource "aws_subnet" "subnet_b" {
-  vpc_id                  = aws_vpc.ecommerce_vpc.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = "ap-south-1b"  # Second AZ
-  map_public_ip_on_launch = true
-}
-
-resource "aws_eks_cluster" "ecommerce_eks" {
-  name     = "ecommerce-cluster"
-  role_arn = "arn:aws:iam::713881815267:role/EKS-Cluster-Role"
-
-  vpc_config {
-    subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id] 
+  tags = {
+    Name = "TerraformInstance"
   }
 }
-
-
